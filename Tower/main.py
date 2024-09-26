bfa816e070562192c9b3e3c7b736887958bf1e89
tuwi-0808374 <0808374@hr.nl>
2024-09-26 15:24:09 +0200

    Part 5 - Placing Turrets

--- Tower/main.py
+++ Tower/main.py
@@ -3,6 +3,7 @@ import constants as c
 import json
 from enemy import Enemy
 from world import World
+from turret import Turret
 
 # init
 pygame.init()
@@ -17,6 +18,8 @@ pygame.display.set_caption("Tower Game")
 # load images
 # Map
 map_image = pygame.image.load("levels/level.png").convert_alpha()
+# Individual turret image for mouse cursor
+cursor_turret = pygame.image.load("assets/images/turrets/cursor_turret.png").convert_alpha()
 # Enemies
 enemy_image = pygame.image.load('assets/images/enemies/enemy_1.png').convert_alpha()
 
@@ -30,12 +33,29 @@ world.process_data()
 
 # create groups
 enemy_group = pygame.sprite.Group()
+turret_group = pygame.sprite.Group()
 
 waypoints = world.waypoints
 
 enemy = Enemy(waypoints, enemy_image)
 enemy_group.add(enemy)
 
+def create_turret(mouse_pos):
+    mouse_tile_x = mouse_pos[0] // c.TILE_SIZE
+    mouse_tile_y = mouse_pos[1] // c.TILE_SIZE
+    # calculate sequential number of tile
+    mouse_tile_num = (mouse_tile_y * c.COLS) + mouse_tile_x
+    # check if tile is grass
+    if world.tile_map[mouse_tile_num] == 7:
+        # Check that there isn't already a turret
+        space_is_free = True
+        for turret in turret_group:
+            if (mouse_tile_x, mouse_tile_y) == (turret.tile_x, turret.tile_y):
+                space_is_free = False
+
+        if space_is_free:
+            turret = Turret(cursor_turret, mouse_tile_x, mouse_tile_y)
+            turret_group.add(turret)
 
 # game loop
 run = True
@@ -56,11 +76,20 @@ while run:
 
     # draw groups
     enemy_group.draw(screen)
+    turret_group.draw(screen)
 
     # event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        # mouse click
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                mouse_pos = pygame.mouse.get_pos()
+
+                # check if mouse is on game area
+                if mouse_pos[0] < c.SCREEN_WIDTH and mouse_pos[1] < c.SCREEN_HEIGHT:
+                    create_turret(mouse_pos)
 
     pygame.display.flip()
 
---
+++ Tower/turret.py
@@ -0,0 +1,14 @@
+import pygame
+import constants as c
+
+class Turret(pygame.sprite.Sprite):
+    def __init__(self, image, tile_x, tile_y):
+        pygame.sprite.Sprite.__init__(self)
+        self.tile_x = tile_x
+        self.tile_y = tile_y
+        # calculate center coordinates
+        self.x = (self.tile_x + .5) * c.TILE_SIZE
+        self.y = (self.tile_y + .5 ) * c.TILE_SIZE
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.x, self.y)
